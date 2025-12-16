1fe8835328b049cd75820c0fe97a2c14dc8507e1
Sachinqodic <sachin@qodictechnosoft.com>
2025-12-16 23:39:21 +0530

    Intial  commit

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Sample variables
+const a = 10;
+const b = 2;
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Server is running successfully",
+    result: a + b
+  });
+});
+
+// Port
+const PORT = 3002;
+
+// Start server
+app.listen(PORT, () => {
+  console.group("Server Info:");
+  console.log(`🚀 Server started and running on port ${PORT}`);
+  console.log(`🌐 Access the server at: http://localhost:${PORT}`);
+  console.groupEnd();
+});
